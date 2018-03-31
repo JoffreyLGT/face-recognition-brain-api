466d153b45f6bd82e4affca1bb10e27f7a7af443
JoffreyLGT <j.lagut@live.fr>
2018-03-31 22:57:51 +0200

    Add a condition in the declaration of the db to transparently select the production or local db

--- server.js
+++ server.js
@@ -9,23 +9,30 @@ const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 const root = require('./controllers/root')
 
-// const db = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'joffrey',
-//     password: 'hello',
-//     database: 'smart-brain'
-//   }
-// })
+let db = null
 
-const db = require('knex')({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  }
-})
+if (process.env.DATABASE_URL) {
+  db = require('knex')({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    }
+  })
+} else {
+  db = require('knex')({
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'joffrey',
+      password: 'hello',
+      database: 'smart-brain'
+    }
+  })
+}
+
+
+
 
 const app = express()
 app.use(cors())
