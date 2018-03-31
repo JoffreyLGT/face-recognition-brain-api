669ecd753fba2cc05217df9c1e03b82f88363713
JoffreyLGT <j.lagut@live.fr>
2018-03-31 22:10:22 +0200

    Add an error handling in register.js when the db is not available

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,6 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body
-  if(!email || !name || !password){
+  if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission')
   }
   const hash = bcrypt.hashSync(password);
@@ -25,7 +25,10 @@ const handleRegister = (req, res, db, bcrypt) => {
           .catch(err => res.status(400).json('Unable to register.'))
       })
       .then(trx.commit)
-      .catch(trx.rollback)
+      .catch(err => {
+        res.status(400).json('Unable to register.')
+        trx.rollback
+      })
   })
 }
 
